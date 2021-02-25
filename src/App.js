5b383f666613b49540651d6713cd59398e553453
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-26 03:28:56 +0600

    Create multiple components from an array of objects

--- src/App.js
+++ src/App.js
@@ -14,13 +14,21 @@ function App() {
     color: 'white',
     backgroundColor: 'goldenrod'
   }
-  const names = ['hannan', 'manosh', 'polash', 'sakib']
+  const names = ['hannan', 'manosh', 'polash', 'sakib', 'onik'];
   //   create object in array
   const products = [
     {name:'Photoshop', price:'$80.99'},
     {name:'Illustrator', price:'50.99'},
-    {name:'PDF Reader', price:'$5.99'}
-  ]
+    {name:'PDF Reader', price:'$5.99'},
+    {name:'Premium EI', price:'$249.99'}
+  ];
+
+  // using maps on data
+  // const productNames = products.map(product => product)
+  // console.log(productNames);
+  // const userNames = names.map(name => name)
+  // console.log(userNames);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -32,10 +40,26 @@ function App() {
         <h3 style={{backgroundColor:'salmon', color:'blue'}}>{institution.university + ", Dept of " + institution.dept}</h3>
         <p>I am a React Developer</p>
 
+        {/* show names dynamically in UI */}
+        <ul>
+          {/* <li>{names[0]}</li> */}
+          {
+            names.map(name => <li>{name}</li>)
+          }
+          {
+            products.map(product => <li>{product.name}</li>)
+          }
+        </ul>
+
+        {/* access object array dynamically */}
+        {
+          products.map(pd => <Product product={pd}></Product>)
+        }
+
         {/* access object array */}
-        <Product product={products[0]}></Product>
+        {/* <Product product={products[0]}></Product>
         <Product product={products[1]}></Product>
-        <Product product={products[2]}></Product>
+        <Product product={products[2]}></Product> */}
         {/* <Product name={products[0].name} price={products[0].price}></Product> */}
 
         {/* pass dynamic data to components */}
@@ -53,19 +77,19 @@ function Product(props){
     border:'1px solid gray',
     borderRadius:'5px',
     backgroundColor:'lightgrey',
-    height:'200px',
-    width:'200px',
+    height:'300px',
+    width:'300px',
     float:'left',
     margin:'10px'
   }
   // console.log(props);
   // destructure for read
   const {name, price} = props.product;
-  console.log(name, price);
+  // console.log(name, price);
   return (
     <div style={productStyle}>
-      <h3>{name}</h3>
-      <h5>{price}</h5>
+      <h5>{name}</h5>
+      <h6>{price}</h6>
       <button>Buy Now</button>
     </div>
   )
